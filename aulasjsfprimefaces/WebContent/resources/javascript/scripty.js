6809ba2d44f6f131918e23c5ffe2218fc1dd9f30
guifroes1984 <guilhermefroes26@gmail.com>
2023-03-04 16:39:11 -0300

    Criando tela de Cidades Parte 5

--- aulasjsfprimefaces/WebContent/resources/javascript/scripty.js
+++ aulasjsfprimefaces/WebContent/resources/javascript/scripty.js
@@ -143,3 +143,41 @@ function ocultarMenu() {
 	  });
 	}
 
+function addFocoAoCampo(campo) {
+	var id = getValorElementPorId(campo);
+	if (id != undefined) {
+		document.getElementById(id).focus();
+	}
+}
+
+function gerenciaTeclaEnter() {
+	$(document).ready(function() {
+		$('input').keypress(function(e) {
+			var code = null;
+			code = (e.keyCode ? e.keyCode : e.which);
+			return (code === 13) ? false : true;
+
+		});
+
+		$('input[type=text]').keydown(function(e) {
+			// Obter o pr�ximo �ndice do elemento de entrada de texto
+			var next_idx = $('input[type=text]').index(this) + 1;
+
+			// Obter o n�mero de elemento de entrada de texto em um documento html
+			var tot_idx = $('body').find('input[type=text]').length;
+
+			// Entra na tecla no c�digo ASCII
+			if (e.keyCode === 13) {
+				if (tot_idx === next_idx)
+					// V� para o primeiro elemento de texto
+					$('input[type=text]:eq(0)').focus();
+				else
+					// V� para o elemento de entrada de texto seguinte
+					$('input[type=text]:eq(' + next_idx + ')').focus();
+			}
+		});
+	});
+
+}
+
+

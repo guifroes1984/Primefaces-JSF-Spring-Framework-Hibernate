79e6d8acb48fd75bd0a74baebf3f840b5aabe73d
guifroes1984 <guilhermefroes26@gmail.com>
2023-03-04 11:42:24 -0300

    Criando tela de Cidades Parte 3

--- aulasjsfprimefaces/src/br/com/project/converters/EstadoConverter.java
+++ aulasjsfprimefaces/src/br/com/project/converters/EstadoConverter.java
@@ -17,7 +17,7 @@ public class EstadoConverter implements Converter, Serializable {
 
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String codigo) {
-		if (codigo != null && codigo.isEmpty()) {
+		if (codigo != null && !codigo.isEmpty()) {
 			return (Estado) HibernateUtil.getCurrentSession().get(Estado.class, new Long(codigo));
 		}
 		return codigo;
@@ -28,9 +28,10 @@ public class EstadoConverter implements Converter, Serializable {
 		if (objeto != null) {
 			Estado c = (Estado) objeto;
 			return c.getEst_id() != null && c.getEst_id() > 0 ? c.getEst_id().toString() : null;
+		} else {
+			return null;
 		}
 
-		return null;
 	}
 
 }
